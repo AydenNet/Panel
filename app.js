409c9a9d5685511d70f48dc8431dbe1d8c16fda1
ExtendsJS <rayandesfossez59000@gmail.com>
2017-11-05 22:08:16 +0100

    Update

--- app.js
+++ app.js
@@ -9,5 +9,11 @@ app.get("/register", (req, res) => res.sendFile(__dirname + "/views/pages/regist
 app.get("/login", (req, res) => res.sendFile(__dirname + "/views/pages/login.html"));
 
 app.post("/register", bodyParser.urlencoded({extended: false}), (req, res) => {
-    
+    const {prenom, mdp, mdp_confirmation} = req.body;
 })
+
+app.post("/login", bodyParser.urlencoded({extended: false}), (req, res) => {
+    const {prenom, mdp} = req.body;
+})
+
+app.listen(8080);
