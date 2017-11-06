4e46b5a41ffe6f20150bf78dcef16d44747f3002
ExtendsJS <rayandesfossez59000@gmail.com>
2017-11-06 21:08:23 +0100

    Update

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
+function verif_register(prenom, mdp, mdp_confirmation){
+    if(prenom === "" || mdp != mdp_confirmation || mdp === ""){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
 app.use(express.static("public"));
 
 app.get("/register", (req, res) => res.sendFile(__dirname + "/views/pages/register.html"));
@@ -9,11 +18,17 @@ app.get("/register", (req, res) => res.sendFile(__dirname + "/views/pages/regist
 app.get("/login", (req, res) => res.sendFile(__dirname + "/views/pages/login.html"));
 
 app.post("/register", bodyParser.urlencoded({extended: false}), (req, res) => {
-    const {prenom, mdp, mdp_confirmation} = req.body;
+    let {prenom, mdp, mdp_confirmation} = req.body;
+    if(verif_register(prenom, mdp, mdp_confirmation) === true){
+        // code bdd
+        res.sendFile(__dirname + "/views/pages/login.html"); 
+    }
+    else res.send("Certaines données sont invalide ! ");
+
 })
 
 app.post("/login", bodyParser.urlencoded({extended: false}), (req, res) => {
-    const {prenom, mdp} = req.body;
+    let {prenom, mdp} = req.body;
 })
 
 app.listen(8080);
